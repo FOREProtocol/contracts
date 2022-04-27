ff89942ab8981dcc566991279d3e6ea1b2c45a1f
Lukasz Dobrowolski <lukasz.dobrowolski@4soft.co>
2022-04-27 15:32:11 +0200

    style: adjust js / ts linter and lint files

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,20 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // Fore Token
-  const ForeTokenArtifact = await ethers.getContractFactory("ForeToken");
-  const foretoken = await ForeTokenArtifact.deploy();
-  await foretoken.deployed();
-  console.log("ForeToken deployed to:", foretoken.address);
+    // Fore Token
+    const ForeTokenArtifact = await ethers.getContractFactory("ForeToken");
+    const foretoken = await ForeTokenArtifact.deploy();
+    await foretoken.deployed();
+    console.log("ForeToken deployed to:", foretoken.address);
 
-  // Vesting
-  const VestingArtifact = await ethers.getContractFactory("ForeVesting");
-  const vesting = await VestingArtifact.deploy(foretoken.address);
-  await vesting.deployed();
-  console.log("Vesting deployed to:", vesting.address);
+    // Vesting
+    const VestingArtifact = await ethers.getContractFactory("ForeVesting");
+    const vesting = await VestingArtifact.deploy(foretoken.address);
+    await vesting.deployed();
+    console.log("Vesting deployed to:", vesting.address);
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
