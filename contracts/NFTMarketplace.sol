2f6d863be6d8008d46b4996f7f14be886944b58c
Lukasz Dobrowolski <lukasz.dobrowolski@4soft.co>
2022-05-06 03:01:05 +0200

    feature: marketplace

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "./external/pancake-nft-markets/ERC721NFTMarketV1.sol";
+
+
+contract NFTMarketplace is
+    ERC721NFTMarketV1
+{
+
+    constructor(
+        address _adminAddress,
+        address _treasuryAddress,
+        address _WBNBAddress,
+        uint256 _minimumAskPrice,
+        uint256 _maximumAskPrice
+    )
+        ERC721NFTMarketV1(
+            _adminAddress,
+            _treasuryAddress,
+            _WBNBAddress,
+            _minimumAskPrice,
+            _maximumAskPrice
+        )
+    {
+    }
+
+}
